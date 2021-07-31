e4ea0a9e6122983eb707e95921f40493b9bd76fd
Ayoub J•シ <ayoub@MBP-de-Ayoub.local.tld>
2021-07-31 19:19:21 +0100

    newest update

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let uncheck=document.querySelector("#checked")
 let tasks = document.querySelector("#tasks");
 let task = document.querySelector(".task");
 let box = document.getElementById("tasks");
+let checkedbox=document.getElementById("checked")
 
 
 const addTask = () => {
@@ -32,25 +33,39 @@ const valid = () =>{
         let input = document.createElement("input");
         let label=document.createElement("label");
         input.setAttribute("type","checkbox");
-        //input.setAttribute("checked");
+        input.setAttribute("checked", "checked");
+        input.setAttribute("onchange","unvalid")
+        input.setAttribute("id","checked-task-box")
         label.appendChild(document.createTextNode(changer));
         uncheck.appendChild(li);
         li.appendChild(input);
         li.appendChild(label);
         console.log(changer);
-        tasks.querySelector('#task-box:checked').parentElement.remove();
-        console.log("")
-        //tasks.querySelector('#task-box:checked').nextElementSibling.remove(li);
-        //tasks.querySelector('#task-box:checked').remove(li);
-
+        tasks.querySelector('#task-box:checked').parentElement.remove(); 
     }
-
-
-
-            
 }
+    const unvalid =()=>{
+
+        if (document.querySelector('#checked-task-box:not(:checked)') !== null) {
+    let newTask = document.querySelector('#checked-task-box:not(:checked)').nextElementSibling.innerHTML;
+    let li = document.createElement("li");
+    let input = document.createElement("input");
+    let label=document.createElement("label");
+    input.setAttribute("type","checkbox");
+    input.setAttribute("onclick","valid");
+    input.setAttribute("id","task-box");
+    li.setAttribute("id","taskk");
+    label.appendChild(document.createTextNode(newTask));
+    tasks.appendChild(li);
+    li.appendChild(input);
+    li.appendChild(label);
+    uncheck.querySelector('#checked-task-box:not(:checked)').parentElement.remove();
+    console.log(newTask);
+        }
+    }
 
 add.addEventListener("click", addTask);
 box.addEventListener("click",valid);
+checkedbox.addEventListener("click",unvalid);
 
 
